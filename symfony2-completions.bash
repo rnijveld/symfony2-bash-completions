f4ed56539315bada540c8234cbae16693c2d0752
Ruben Nijveld <ruben@gewooniets.nl>
2013-11-16 07:54:40 +0100

    Updated completions, should be more reliable now

--- symfony2-completions.bash
+++ symfony2-completions.bash
@@ -1,9 +1,9 @@
 _symfony2_console_complete()
 {
     if [ $COMP_CWORD -gt 1 ]; then
-        local cmd_list=`${COMP_WORDS[0]} help ${COMP_WORDS[1]} --raw | sed -e '1,/Options:/d' | awk '{ print $1 }'`
+        local cmd_list=`${COMP_WORDS[0]} help ${COMP_WORDS[1]} --raw | awk '/^ ?--.*$/ { print $1 }'`
     else
-        local cmd_list=`${COMP_WORDS[0]} list --raw | sed 1d | awk '{ print $1 }'`
+        local cmd_list=`${COMP_WORDS[0]} list --raw | awk '{ print $1 }'`
     fi
     local cur=${COMP_WORDS[COMP_CWORD]}
     COMPREPLY=( $(compgen -W "$cmd_list" -- $cur) )
