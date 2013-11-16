6581906d96ef6b28e728ade5eb7ca5b83d2e94ff
Ruben Nijveld <ruben@gewooniets.nl>
2013-11-16 09:01:00 +0100

    Added composer to completions, disable ansi colors for completions

--- symfony2-completions.bash
+++ symfony2-completions.bash
@@ -1,9 +1,9 @@
 _symfony2_console_complete()
 {
     if [ $COMP_CWORD -gt 1 ]; then
-        local cmd_list=`${COMP_WORDS[0]} help ${COMP_WORDS[1]} --raw | awk '/^ ?--.*$/ { print $1 }'`
+        local cmd_list=`${COMP_WORDS[0]} help ${COMP_WORDS[1]} --raw --no-ansi | awk '/^ ?--.*$/ { print $1 }'`
     else
-        local cmd_list=`${COMP_WORDS[0]} list | awk '/^  [^[-].*/ { print $1 }'`
+        local cmd_list=`${COMP_WORDS[0]} list --no-ansi | awk '/^  [a-z].*/ { print $1 }'`
     fi
     local cur=${COMP_WORDS[COMP_CWORD]}
     COMPREPLY=( $(compgen -W "$cmd_list" -- $cur) )
@@ -11,3 +11,4 @@ _symfony2_console_complete()
 
 complete -F _symfony2_console_complete app/console
 complete -F _symfony2_console_complete symfony
+complete -F _symfony2_console_complete composer
